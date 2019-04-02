432b7d3fd10ca363f504ba710a19c416b878afaa
Hélio Rocha <hjrocha@gmail.com>
2019-04-02 14:29:55 +0100

    increase duplication

--- codacy_demo_py/views.py
+++ codacy_demo_py/views.py
@@ -2,8 +2,14 @@ from django.http import HttpResponse
 
 
 def index(request):
+    a = "first line"
+    b = "second line"
+    c = "other line..."
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-def other_index(request):
+def other_index(request):a = "first line"
+    a = "first line"
+    b = "second line"
+    c = "other line..."
     return HttpResponse("Hello, world. You're at the polls index.")
