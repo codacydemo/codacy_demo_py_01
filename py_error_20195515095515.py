88c4e5e32d67dddf68052652974fc4db80ee683e
Hélio Rocha <hjrocha@gmail.com>
2019-04-15 09:55:15 +0100

    Added js_error_20195515095515.js, py_xss_20195515095515.py, py_error_20195515095515.py

---
+++ py_error_20195515095515.py
@@ -0,0 +1,31 @@
+class C20195515095515():
+    "This is an example class"
+    a = 10
+    def func(self):
+        print('Hello Example')
+
+# Output: 10
+print(PythonExample.a)
+
+# Output: <function MyClass.func at 0x0000000003079BF8>
+print(PythonExample.func)
+
+# Output: 'This is my second class'
+print(PythonExample.__doc__)
+
+# Code Injection
+def GET(self):
+    get_input = web.input()
+    param1 = get_input['param1'] if 'param1' in get_input else None
+    if (param1):
+        x = eval(param1)
+    return "I'm vulnerable"
+
+# CWE-759
+def storePassword(userName,Password):
+    hasher = hashlib.new('md5')
+    hasher.update(Password)
+    hashedPassword = hasher.digest()
+
+    # UpdateUserLogin returns True on success, False otherwise
+    return updateUserLogin(userName,hashedPassword)
