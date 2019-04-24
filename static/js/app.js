bd4ad5e58ed7c0ea6a73b3d433cd3e8a35d07fdd
Hélio Rocha <hjrocha@gmail.com>
2019-04-24 17:31:44 +0100

    add complexity

--- static/js/app.js
+++ static/js/app.js
@@ -4,5 +4,20 @@ function hello() {
 function world() {
     return "World";
 }
+
+function aVeryComplexMethod(isOk) {
+    if (isOk) {
+        call(hello, function (sender) {
+            setTimeout(function () {
+                console.log(sender.caller);
+            }, 500);
+        });
+    }
+}
+
+function call(subject, callback) {
+    alert(`Calling my ${subject}.`);
+    callback({ caller: 'method' });
+}
 // Basic export
 export { hello, world };
