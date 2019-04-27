54d67eb6c5ba846aa9ffd15bec252713125da13d
Hélio Rocha <hjrocha@gmail.com>
2019-04-27 11:57:10 +0100

    remove extra js and add complex python

---
+++ complex01.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def other_index(request):
+    for x in range(0, 3):
+        print("We're on time %d" % (x))
+        y = 1
+        while True:
+            if(y%2 ==0):
+                print("To infinity and beyond! We're getting close, on %d now!" % (y))
+            y += 1  
+    return HttpResponse("Hello, world. You're at the polls index.")
