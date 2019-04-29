2b3f33868b91bdd59e7ae7893d1b83def1cbccdf
Hélio Rocha <hjrocha@gmail.com>
2019-04-29 11:39:28 +0100

    js complexity

--- static/js/utils.js
+++ static/js/utils.js
@@ -4,10 +4,30 @@ function hello() {
     let newline2 = ""
     return "Hello";
 }
+
+
 function world() {
     let a = "dummy"
     let newline = ""
     let newline2 = ""
     let b = "line"
     return "World";
+}
+
+function complex(isOk, totalItems) {
+    let result = 0
+    if(isOk){
+        for(let i = 0; i < totalItems; i++){
+            let j = 0;
+            while(j < 10){
+                if(j % 2 == 0){
+                    result += j*i;
+                    if(result % 10 == 0){
+                        world();
+                    }
+                }
+                j++;
+            }
+        }
+    }
 }
