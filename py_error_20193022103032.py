c599533cd64525f5fb19509716cc9d9db34eea1d
Hélio Rocha <hjrocha@gmail.com>
2019-04-29 10:46:40 +0100

    Fixed Python and Javascript errors

--- py_error_20193022103032.py
+++ py_error_20193022103032.py
@@ -30,3 +30,13 @@ def storePassword(userName,Password):
 
     # UpdateUserLogin returns True on success, False otherwise
     return updateUserLogin(userName,hashedPassword)
+
+def calc(number,times):
+    for x in range(0, 3):
+        print("We're on time %d" % (x))
+        y = 1
+        while True:
+            if(y%2 ==0):
+                print("To infinity and beyond! We're getting close, on %d now!" % (y))
+            y += 1
+    return number*times
