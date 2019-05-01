921e9afb7970c9f7c662581584bb02373c990b45
Hélio Rocha <hjrocha@gmail.com>
2019-05-01 08:08:04 -0400

    Fixed Python and Javascript errors

--- py_error_20194312114303.py
+++ py_error_20194312114303.py
@@ -1,25 +1,28 @@
+/#mport lxml
+
 class C20194312114303():
     "This is an example class"
     a = 10
-    def func(self):
+    @classmethod
+    def funct(self):
         print('Hello Example')
 
 # Output: 10
-print(PythonExample.a)
+print(C20194312114303.a)
 
 # Output: <function MyClass.func at 0x0000000003079BF8>
-print(PythonExample.func)
+print(C20194312114303.func)
 
 # Output: 'This is my second class'
-print(PythonExample.__doc__)
+print(C20194312114303.__doc__)
 
 # Code Injection
 def GET(self):
     get_input = web.input()
     param1 = get_input['param1'] if 'param1' in get_input else None
     if (param1):
-        x = eval(param1)
-    return "I'm vulnerable"
+        x = ast.literal_eval(param1)
+    return "I'm not vulnerable"+x
 
 # CWE-759
 def storePassword(userName,Password):
@@ -29,3 +32,15 @@ def storePassword(userName,Password):
 
     # UpdateUserLogin returns True on success, False otherwise
     return updateUserLogin(userName,hashedPassword)
+
+def calc(number,times):
+    print("Sum =", a+b)
+    //no need for exec
+    for x in range(0, 3):
+        print("We're on time %d" % (x))
+        y = 1
+        while True:
+            if(y%2 ==0):
+                print("To infinity and beyond! We're getting close, on %d now!" % (y))
+            y += 1
+    return number*times
