0c7aa2a790bf441bca0212719254b13c6d7aba41
Hélio Rocha <hjrocha@gmail.com>
2019-12-03 07:47:03 +0000

    fib sequence

--- py_error_20195715025739.py
+++ py_error_20195715025739.py
@@ -47,3 +47,22 @@ def calc_old_thing(number,times):
                 print("To infinity and beyond! We're getting close, on %d now!" % (y))
             y += 1
     return number*times
+
+def fib(nterms):
+    n1, n2 = 0, 1
+    count = 0
+    # check if the number of terms is valid
+    if nterms <= 0:
+        print("Please enter a positive integer")
+    elif nterms == 1:
+        print("Fibonacci sequence upto",nterms,":")
+        print(n1)
+    else:
+        print("Fibonacci sequence:")
+        while count < nterms:
+            print(n1)
+            nth = n1 + n2
+            # update values
+            n1 = n2
+            n2 = nth
+            count += 1
